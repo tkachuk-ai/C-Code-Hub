ea205de0d2c6eb62eeb18b35395c00cb6a8eeafc
Oleksandra Tkachuk <144816647+tkachuk-ai@users.noreply.github.com>
2024-02-06 19:51:24 -0500

    Create HelloWorld.c

---
+++ ANSI-C/HelloWorld.c
@@ -0,0 +1,8 @@
+/* Prints a line of text */
+#include <stdio.h>  // A preprocessor directive (stdio.h -> a standard input/output header)
+
+// Function main begins execution
+int main(){
+  printf("Hello, World!\n");
+} // end of function main
+
